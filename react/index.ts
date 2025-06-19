21866fdf71b01c8574ff47730e4deffb39408284
Jeremy Klas <65559301+jereklas@users.noreply.github.com>
2025-06-19 06:58:44 -0700

    add controller aliases to react package

--- react/index.ts
+++ react/index.ts
@@ -1,3 +1,8 @@
 export { observer } from "./hof/observer.ts"
 export { getGlobalState } from "./state/global.ts"
 export * from "./hooks/useViewModel.ts"
+// Alias VM to Controller
+export {
+  useViewModel as useController,
+  ViewModel as Controller,
+} from "./hooks/useViewModel.ts"

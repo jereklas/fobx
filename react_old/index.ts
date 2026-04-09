4ca7c6d29b77453c3f02683ba8e97fe6e1e8159d
Jeremy Klas <65559301+jereklas@users.noreply.github.com>
2026-04-09 00:14:18 -0700

    remove old code

--- react_old/index.ts
+++
@@ -1,8 +0,0 @@
-export { observer } from "./hof/observer.ts"
-export { getGlobalState } from "./state/global.ts"
-export * from "./hooks/useViewModel.ts"
-// Alias VM to Controller
-export {
-  useViewModel as useController,
-  ViewModel as Controller,
-} from "./hooks/useViewModel.ts"
